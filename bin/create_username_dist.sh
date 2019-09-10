15fdc6c6ab79e66f9b03e3b3009c014d0955c643
unwarysage <shawx538@morris.umn.edu>
2019-09-10 15:51:07 -0500

    Follow shellchecks style`

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
 WORK_PATH="$1"
-START_PATH=$(pwd)
 
-cd "$WORK_PATH"
+#Do all the work in the scratch directory in a subshell
+(cd "$WORK_PATH" || exit
 
 for directory in ./*; do
- 	cd "$directory"
-  	sed -E "s/[A-Za-z]+ [0-9]+ [0-9]+ ([a-zA-Z]+) [a-zA-Z0-9. ]+/\1/;t;d" < "failed_login_data.txt" > "../${directory}_data.txt"
-	cd .. 
+	#use a subshell to go into the directory
+ 	(cd "$directory" || exit
+  	sed -E "s/[A-Za-z]+ [0-9]+ [0-9]+ ([a-zA-Z]+) [a-zA-Z0-9. ]+/\1/;t;d" < "failed_login_data.txt" > "../${directory}_data.txt") 
 
 done
 #Combine the data from all machines into one file
 for file in ./*_data.txt; do
-  cat $file >> "combined.txt"
+  cat "$file" >> "combined.txt"
 done
 
 #Thanks to https://stackoverflow.com/questions/15984414/bash-script-count-unique-lines-in-file
@@ -21,9 +21,8 @@ done
 sort "combined.txt" | uniq -c | sort -bgr > "combined_data.txt"
 
 #now we reformat them into js commands
-sed -E "s/ +([0-9]+) ([a-zA-Z\-_0-9]+)/data\.addRow\(\['\2', \1\]\);/" < "combined_data.txt" > "combined_js.txt"
+sed -E "s/ +([0-9]+) ([a-zA-Z\-_0-9]+)/data\.addRow\(\['\2', \1\]\);/" < "combined_data.txt" > "combined_js.txt")
 
-cd "$START_PATH"
 
 ./bin/wrap_contents.sh "${WORK_PATH}/combined_js.txt" "./html_components/username_dist" "${WORK_PATH}/username_dist.html"
 
