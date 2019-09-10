d40559e2f644af660d32775dfa17e04193feeff5
corde171 <corde171@morris.umn.edu>
2019-09-10 08:06:21 -0500

    Finish process_client_logs.sh by adding another regex expression for special case.

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 WORK_PATH=$1
 
-EXP1="([a-zA-Z]+ [0-9]+ [0-9]+):[0-9]+:[0-9]+ ([a-zA-Z_]+) sshd\[[0-9]+\]: Failed password for invalid user ([a-zA-Z0-9_]+) from ([0-9\.]+)"
-EXP2="([a-zA-Z]+ [0-9]+ [0-9]+):[0-9]+:[0-9]+ ([a-zA-Z_]+) sshd\[[0-9]+\]: Failed password for ([a-zA-Z0-9_]+) from ([0-9\.]+)"
+EXP1="([a-zA-Z]+ [0-9]+ [0-9]+):[0-9]+:[0-9]+ ([a-zA-Z_]+) sshd\[[0-9]+\]: Failed password for invalid user ([a-zA-Z0-9_]+) from ([0-9.]+).*"
+EXP2="([a-zA-Z]+ [0-9]+ [0-9]+):[0-9]+:[0-9]+ ([a-zA-Z_]+) sshd\[[0-9]+\]: Failed password for ([a-zA-Z0-9_]+) from ([[0-9.]+).*"
+EXP3="([a-zA-Z]+  [0-9]+ [0-9]+):[0-9]+:[0-9]+ ([a-zA-Z_]+) sshd\[[0-9]+\]: Failed password for root from ([0-9\.]+).*"
 cd "$WORK_PATH" || exit
 touch "failed_login_data.txt"
 #chmod 733 "failed_login_data.txt"
@@ -11,4 +12,5 @@ for filename in ./var/log/*; do
   [ -e "$filename" ] || continue
   sed -E "s/$EXP1/\1 \3 \4/;t;d" "$filename" >> "failed_login_data.txt"
   sed -E "s/$EXP2/\1 \3 \4/;t;d" "$filename" >> "failed_login_data.txt"
+  sed -E "s/$EXP3/\1 root \3/;t;d" "$filename" >> "failed_login_data.txt"
 done
