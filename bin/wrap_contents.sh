f598412bdf558d19d7fdd2615c0533903ab28265
unwarysage <unwary@comcast.net>
2019-09-18 23:15:42 -0500

    Reformat wrap_contents for shellcheck

--- bin/wrap_contents.sh
+++ bin/wrap_contents.sh
@@ -6,7 +6,7 @@ WRAPPER_NAME="$2"
 OUTPUT_NAME="$3"
 
 #create our output file
-touch $OUTPUT_NAME
+touch "$OUTPUT_NAME"
 
 #make the header and footer names
 #string concatenation is a two step process it seems
@@ -19,8 +19,9 @@ FOOTER_NAME+="_footer.html"
 
 #append the contents of the header, then the middle bit, then the footer
 
-echo "$(cat $HEADER_NAME)" >> $OUTPUT_NAME
-echo "$(cat $CONTENTS_FILE)" >> $OUTPUT_NAME
-echo "$(cat $FOOTER_NAME)" >> $OUTPUT_NAME
+
+
+cat "$HEADER_NAME" "$CONTENTS_FILE" "$FOOTER_NAME" >> "$OUTPUT_NAME"
+
 
 #done
