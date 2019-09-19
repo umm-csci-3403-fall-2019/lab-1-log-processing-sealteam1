1319f53b5e39f1118181119c4b9631430b37258f
unwarysage <shawx538@morris.umn.edu>
2019-09-19 14:49:53 -0500

    Fix regex issue with hours dist sh

--- bin/create_hours_dist.sh
+++ bin/create_hours_dist.sh
@@ -11,7 +11,7 @@ for directory in ./*; do
   if [[ -d "$directory" ]]; then
    	cd "$directory" || exit
         #put those under a different file extension to not have later proccessing stages get picked up by even later scripts
-    	sed -E "s/[A-Za-z]+ {1,2}[0-9]+ ([0-9]+) [A-Za-z0-9_\-]+ [0-9\.]+ [a-z0-9 ]+/\1/;t;d" < "failed_login_data.txt" > "../${directory}_hours_data.src"
+    	sed -E "s/[A-Za-z]+ +[0-9]+ ([0-9]+) [a-zA-Z0-9_\-]+ [0-9. ]+/\1/;t;d" < "failed_login_data.txt" > "../${directory}_hours_data.src"
         #jump back up to the higher folder
         cd ../ || exit
   fi
