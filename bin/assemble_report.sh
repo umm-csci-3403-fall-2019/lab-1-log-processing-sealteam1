d5f3ac00c30ac30209c74bf1c926c731808f21fc
unwarysage <unwary@comcast.net>
2019-09-18 23:03:50 -0500

    Remove unecessary subshell from assemble reports

--- bin/assemble_report.sh
+++ bin/assemble_report.sh
@@ -3,8 +3,7 @@
 DIST_DIR="$1"
 WKDIR=$(pwd)
 SCRIPT_PATH="$WKDIR"/bin
-(cd "$DIST_DIR" || exit
+cd "$DIST_DIR" || exit
 cat "country_dist.html" "hours_dist.html" "username_dist.html" > "total_dist.html"
 
-"$SCRIPT_PATH"/wrap_contents.sh "total_dist.html" "$WKDIR/html_components/summary_plots" "failed_login_summary.html"
-)
+"$SCRIPT_PATH/wrap_contents.sh" "total_dist.html" "$WKDIR/html_components/summary_plots" "failed_login_summary.html"
